4fbf30f7562d3a4da4764b0fce15c2f100f11fd2
DanGould <d@ngould.dev>
2024-01-23 17:56:44 -0500

    Do `ohttp_relay`

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,57 @@
-use std::convert::Infallible;
 use std::net::SocketAddr;
 
-use http_body_util::Full;
-use hyper::body::Bytes;
+use hyper::body::Incoming;
+use hyper::header::{CONTENT_LENGTH, CONTENT_TYPE, HOST};
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 
-async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+const PAYJO_IN: &str = "payjo.in";
+const OHTTP_RELAY_HOST: &str = "localhost";
+
+async fn ohttp_relay(
+    mut req: Request<hyper::body::Incoming>,
+) -> Result<Response<Incoming>, hyper::Error> {
+    let content_type_header = req.headers().get(CONTENT_TYPE).cloned();
+    let content_length_header = req.headers().get(CONTENT_LENGTH).cloned();
+    req.headers_mut().clear();
+    req.headers_mut().insert(HOST, OHTTP_RELAY_HOST.parse().unwrap());
+    if let Some(content_type) = content_type_header {
+        req.headers_mut().insert(CONTENT_TYPE, content_type);
+    }
+    if let Some(content_length) = content_length_header {
+        req.headers_mut().insert(CONTENT_LENGTH, content_length);
+    }
+
+    let uri_string = format!(
+        "http://{}{}",
+        PAYJO_IN,
+        req.uri().path_and_query().map(|x| x.as_str()).unwrap_or("/")
+    );
+    let uri = uri_string.parse().unwrap();
+    println!("uri: {:?}", uri);
+    *req.uri_mut() = uri;
+
+    let host = req.uri().host().expect("uri has no host");
+    let port = req.uri().port_u16().unwrap_or(80);
+    let addr = format!("{}:{}", host, port);
+
+    async move {
+        let client_stream = TcpStream::connect(addr).await.unwrap();
+        let io = TokioIo::new(client_stream);
+
+        let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
+        tokio::task::spawn(async move {
+            if let Err(err) = conn.await {
+                println!("Connection failed: {:?}", err);
+            }
+        });
+
+        sender.send_request(req).await
+    }
+    .await
 }
 
 #[tokio::main]
@@ -24,7 +65,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let io = TokioIo::new(stream);
 
         tokio::task::spawn(async move {
-            if let Err(err) = http1::Builder::new().serve_connection(io, service_fn(hello)).await {
+            if let Err(err) =
+                http1::Builder::new().serve_connection(io, service_fn(ohttp_relay)).await
+            {
                 println!("Error serving connection: {:?}", err);
             }
         });
