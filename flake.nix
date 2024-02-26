e24f8e4d520c8ad4eceead82bee42918130fa287
DanGould <d@ngould.dev>
2024-02-26 15:42:43 -0500

    Add custom nginx to flake

--- flake.nix
+++ flake.nix
@@ -3,32 +3,40 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
-
     crane = {
       url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    nginx-with-stream = {
+      url = "path:./nginx-with-stream";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+  outputs = { self, nixpkgs, crane, nginx-with-stream, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
         craneLib = crane.lib.${system};
+
+        nginxWithStream = nginx-with-stream.packages.${system}.nginx-with-stream;
+
         ohttp-relay = craneLib.buildPackage {
           src = craneLib.cleanCargoSource (craneLib.path ./.);
           strictDeps = true;
 
           buildInputs = [
-            # Add additional build inputs here
+            nginxWithStream
           ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
             # Additional darwin specific inputs can be set here
             pkgs.libiconv
             pkgs.darwin.apple_sdk.frameworks.Security
           ];
+
+          preBuild = ''
+            export PATH=${nginxWithStream}/bin:$PATH
+          '';
 
           # Additional environment variables can be set directly
           # MY_CUSTOM_VAR = "some value";
@@ -54,7 +62,7 @@
 
           # Extra inputs can be added here; cargo and rustc are provided by default.
           packages = [
-            # pkgs.ripgrep
+            nginxWithStream
           ];
         };
       });
