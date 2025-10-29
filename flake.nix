14b119eb86949da4181d28530709f2f0ce45cf36
Yuval Kogman <nothingmuch@woobling.org>
2025-10-29 19:31:38 +0100

    update to nixpkgs stable (25.05)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Build a cargo project without extra checks";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
     crane = {
       url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
