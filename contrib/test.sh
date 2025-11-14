7e385c6f406a77a47354fd681a788f3e97617d85
user <benalleng@gmail.com>
2025-11-14 14:52:14 -0500

    Create more expandable test infra

---
+++ contrib/test.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+set -e
+
+LOCKFILE="Cargo.lock"
+LOCKDIR=".bak"
+LOCKFILE_BAK="${LOCKDIR}/${LOCKFILE}"
+
+cleanup() {
+    if [ -f "$LOCKFILE_BAK" ]; then
+        mv "$LOCKFILE_BAK" "$LOCKFILE"
+    fi
+    rmdir "$LOCKDIR"
+}
+
+if ! mkdir "$LOCKDIR" 2>/dev/null; then
+    echo "Another instance is running. If you're sure it's not, remove $LOCKDIR and try again." >&2
+    exit 1
+fi
+
+trap cleanup EXIT
+
+if [ -f "$LOCKFILE" ]; then
+    mv "$LOCKFILE" "$LOCKFILE_BAK"
+fi
+
+DEPS="recent minimal"
+
+for dep in $DEPS; do
+    cargo --version
+    rustc --version
+
+    # Some tests require certain toolchain types.
+    export NIGHTLY=false
+    export STABLE=true
+    if cargo --version | grep nightly; then
+        STABLE=false
+        NIGHTLY=true
+    fi
+    if cargo --version | grep beta; then
+        STABLE=false
+    fi
+
+    cp "Cargo-$dep.lock" "$LOCKFILE"
+
+    cargo test --locked --package ohttp-relay --verbose --lib
+    cargo test --locked --package ohttp-relay --verbose --test integration
+
+    cargo test --locked --package ohttp-relay --verbose --all-features --lib
+    cargo test --locked --package ohttp-relay --verbose --all-features --test integration
+done
